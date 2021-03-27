43fd254dadf30aa323ed94c7a624174646972142
JorgeFelipejf <jorgefelipe.jf01@gmail.com>
2021-03-26 23:20:24 -0300

    :package:menu sidebar

---
+++ script.js
@@ -0,0 +1,5 @@
+var monsterburguer = document.querySelector(".monsterburguer");
+
+monsterburguer.addEventListener("click", function () {
+    document.querySelector(".container").classList.toggle("show-menu");
+});
